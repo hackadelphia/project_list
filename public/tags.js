430a584e4673b6e82534dfeba9fe305c4b424942
Erik Hollensbe <erik@hollensbe.org>
2010-08-13 15:54:58 -0400

    * Have the tag search working the way I like it, I think.

--- public/tags.js
+++ public/tags.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-  // XXX with exception to the ajax call that gets the items, ripped wholesale from jquery ui docs.
-  var availableTags = [];
-  $.getJSON('/ajax/list/techs', {}, function(data) { availableTags = data });
-
   function split(val) {
     return val.split(/\s*,\s*/);
   }
@@ -11,7 +7,11 @@ $(document).ready(function() {
     return split(term).pop();
   }
   
-  $(".tech_complete").autocomplete({
+  // XXX with exception to the ajax call that gets the items, ripped wholesale from jquery ui docs.
+  var availableTags = [];
+  $.getJSON('/ajax/list/techs', {}, function(data) { availableTags = data });
+
+  $("#search-tech, .tech_complete").autocomplete({
     minLength: 2,
     source: function(request, response) {
       // delegate back to autocomplete, but extract the last term
