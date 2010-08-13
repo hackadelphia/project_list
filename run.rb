38636d56a225607c39062ab632dc395f00609746
Erik Hollensbe <erik@hollensbe.org>
2010-08-13 16:27:59 -0400

    * Search is working!

--- run.rb
+++ run.rb
@@ -99,6 +99,22 @@ get '/' do
   haml :index
 end
 
+post '/search' do
+  @title = "Search Results"
+  techs = params[:techs].split(/\s*,\s*/)
+  @full_projects = if !params[:name].empty? and !techs.empty?
+                     $db.search_project_by_name_and_tech(params[:name], techs)
+                   elsif !params[:name].empty?
+                     $db.search_project_by_name(params[:name])
+                   elsif !techs.empty?
+                     $db.search_project_by_tech(techs)
+                   else
+                     $db.projects
+                   end
+
+  haml :index
+end
+
 #
 # Ajax routes (autocompletion, etc)
 #
